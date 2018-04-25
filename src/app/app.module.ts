50034c89065b4f62c4da78e0a87123d3590bbabc
harshivvp <pandyaharshiv@gmail.com>
2018-04-25 20:01:54 +0530

    commitcommitcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages.components';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent, MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/messages.components.ts
+++ src/app/messages.components.ts
@@ -1,12 +1,19 @@
 import { Component } from "@angular/core";
+import { NgForOf } from "@angular/common";
 
 @Component({
     selector: "messages",
-    template: "<h1>This is the message component!</h1>", 
-
+    template: `
+    <h1>This is the message component!</h1>
+    <div *ngFor='let message of messages'>
+        {{ message.owner }} says :- {{ message.text }}
+    </div>
+    <button>Basic</button>
+    `
 })
 
 export class MessagesComponent {    
-    messages = [{text:"some random text", owner:"Kunal"},{text:"other random text", owner:"Nilesh"}];
-}
+    messages = [{text:"Hello Kiddo!", owner:"Kunal"},{text:"Nah. Bye!", owner:"Nilesh"}];
 
+}
+ 
