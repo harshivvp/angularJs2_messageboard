dac2dd03f9495d1807d382fbf8534f3cc5d76928
harshivvp <pandyaharshiv@gmail.com>
2018-04-26 18:14:04 +0530

    added webservice / api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages.components';
-
+import { WebService } from './web.service';
+import { HttpModule } from '@angular/http'
 
 @NgModule({
   declarations: [
-    AppComponent, MessagesComponent
+    AppComponent, MessagesComponent,
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule
+    BrowserModule, BrowserAnimationsModule, HttpModule
   ],
-  providers: [],
+  providers: [WebService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/messages.components.ts
+++ src/app/messages.components.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForOf } from "@angular/common";
-
+import { WebService } from "./web.service";
 
 @Component({
     selector: "messages",
@@ -20,8 +20,16 @@ import { NgForOf } from "@angular/common";
         `
 })
 
-export class MessagesComponent {    
-    messages = [{text:"Hello kid!", owner:"Kunal"},{text:"lol!", owner:"Nilesh"}];
+export class MessagesComponent {
 
+    constructor(private webService: WebService){}
+
+    async ngOnInit() {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+        var response = await this.webService.getMessages(); 
+        this.messages = response.json();
+    }
+    messages = [{text:"Hello kid!", owner:"Kunal"},{text:"lol!", owner:"Nilesh"}];
 }
  
